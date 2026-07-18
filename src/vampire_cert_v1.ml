1c2c7b5fc2d362655ff1339fb21262a4426b5718
Alice <Alice@mail.com>
2026-07-18 01:22:59 +0000

    Normalize closed Skolem definitions

--- src/vampire_cert_v1.ml
+++ src/vampire_cert_v1.ml
@@ -13976,9 +13976,6 @@ let elaborate_core_resolution_refutation_native
   let variables = native_core_proof_variables ~exclude_names:external_definition_names cert in
   let symbol_table = native_core_symbol_table cert in
   let proof_delta, raw_definition_delta = native_core_certificate_sgdelta cert symbol_table in
-  let normalize_generated_skolems =
-    native_core_expand_generated_skolems_tm cert raw_definition_delta
-  in
   let typed_steps =
     List.map
       (native_core_type_raw_equalities_step cert variables symbol_table)
@@ -14038,6 +14035,9 @@ let elaborate_core_resolution_refutation_native
   in
   let proof_delta = native_core_close_delta_table variables proof_delta in
   let definition_delta = native_core_close_delta_table variables raw_definition_delta in
+  let normalize_generated_skolems =
+    native_core_expand_generated_skolems_tm cert definition_delta
+  in
   let proof_delta = native_core_merge_external_delta proof_delta external_delta_table in
   let check_step_proof id clause proof =
     let step_variables = native_core_step_variables cert id in
@@ -14434,9 +14434,6 @@ let elaborate_preprocess_refutation_native
   let variables = native_core_proof_variables ~exclude_names:external_definition_names cert in
   let symbol_table = native_core_symbol_table cert in
   let proof_delta, raw_definition_delta = native_core_certificate_sgdelta cert symbol_table in
-  let normalize_generated_skolems =
-    native_core_expand_generated_skolems_tm cert raw_definition_delta
-  in
   let typed_steps =
     List.map
       (native_core_type_raw_equalities_step cert variables symbol_table)
@@ -14496,6 +14493,9 @@ let elaborate_preprocess_refutation_native
   in
   let proof_delta = native_core_close_delta_table variables proof_delta in
   let definition_delta = native_core_close_delta_table variables raw_definition_delta in
+  let normalize_generated_skolems =
+    native_core_expand_generated_skolems_tm cert definition_delta
+  in
   let proof_delta = native_core_merge_external_delta proof_delta external_delta_table in
   let check_step_proof id prop proof =
     let step_variables = native_core_step_variables cert id in
